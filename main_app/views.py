419f7db462c29658a1dad701fa404dc620307b8a
grantmn11 <grantferreira11@gmail.com>
2021-04-28 09:33:05 -0700

    adds

--- main_app/views.py
+++ main_app/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect
 
 from .models import Parent, Summer
 from django.views.generic.edit import CreateView
-# from multiselectfield import MultiSelectField
+from multiselectfield import MultiSelectField
 
 
 # Create your views here.
